e1fff1d1227683d2dcde1cd0c0d83f90eac278df
jein-sh <82212762+jein-sh@users.noreply.github.com>
2021-07-05 07:45:57 +0200

    оживление интерфейса

--- search.js
+++ search.js
@@ -1,12 +1,44 @@
 const buttonSearch = document.querySelector(".button-search");
 const searchPopup = document.querySelector(".search-form-popup");
-const arrivalDate = searchPopup.querySelector("[name=arrival-date]")
+const searchForm = searchPopup.querySelector(".search-form-intro");
+const arrivalDate = searchPopup.querySelector("[name=arrival-date]");
+const departureDate = searchPopup.querySelector("[name=departure-date]");
+const numChildren = searchPopup.querySelector("[name=num-children]");
+const numAdults = searchPopup.querySelector("[name=num-adults]");
+
+document.addEventListener("DOMContentLoaded", function() {
+  searchPopup.classList.remove("popup-show")
+  searchPopup.classList.add("popup-close")
+});
 
 buttonSearch.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    searchPopup.classList.toggle("popup-show");
-    searchPopup.classList.toggle("popup-close")
-    arrivalDate.focus()
+  if (searchPopup.classList.contains("popup-error")) {
+      evt.preventDefault();
+      searchPopup.classList.remove("popup-error");}
+  evt.preventDefault();
+  searchPopup.classList.toggle("popup-show");
+  searchPopup.classList.toggle("popup-close");
+  arrivalDate.focus();
+});
 
-  });
+searchForm.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !departureDate.value || !numAdults.value || !numChildren) {
+    evt.preventDefault();
+    searchPopup.classList.remove("popup-error")
+    searchPopup.offsetWidth = searchPopup.offsetWidth;
+    searchPopup.classList.add("popup-error");
+    arrivalDate.focus()
+   }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.key === "Esc" || evt.key === "Escape") {
+    if (searchPopup.classList.contains("popup-show")) {
+      evt.preventDefault();
+      searchPopup.classList.remove("popup-show");
+      searchPopup.classList.remove("popup-error")
+      searchPopup.classList.add("popup-close");
+    }
+  }
+});
 
